c9da687b06db6edb2e545abb18c86fb4966bd422
Bill Leonhardt <iamleonhardt@gmail.com>
2016-12-21 21:27:48 -0800

    created multiple elements in fighters

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function Fighter(name) {
     this.atk = function (opponent) {
         var ranDmg = this.atkDmg(10);
         opponent.hitpoints -= ranDmg;
+        $('#'+opponent.name).text(opponent.name + ' ' + opponent.hitpoints);
         console.log(this.name + ' hits ' + opponent.name + ' for ' + ranDmg + '.');
         console.log(opponent.name + ' has ' + opponent.hitpoints + ' left.');
         if (opponent.hitpoints <= 0) {
@@ -26,6 +27,8 @@ function Fighter(name) {
         opponent.hitpoints -= ranDmg;
         var flavor = [' winds up with all he has and smacks ', ' uses all his rage and smashes ', ' pulls out a secret weapon and hits '];
         var ranFlavor = Math.floor(Math.random() * flavor.length);
+        opponentDiv = '#'+opponent.name;
+        $('#Sean').text(opponent.name + ' ' + opponent.hitpoints);
         console.log(this.name + flavor[ranFlavor] + opponent.name + ' for ' + ranDmg + '.');
         console.log(opponent.name + ' has ' + opponent.hitpoints + ' left.');
         if (opponent.hitpoints <= 0) {
@@ -41,8 +44,17 @@ function Fighter(name) {
     this.createDOMElement = function(){
         this.domElement = $("<div>",{
             class: 'fighterElement',
-            text: this.name + ' ' + this.hitpoints
+            id: this.name
         });
+        var nameDiv = $('<div>', {
+            class: 'fighterName',
+            text: this.name
+        });
+        var hpDiv = $('<div>', {
+            class: 'fighterHP',
+            text: this.hitpoints
+        });
+        this.domElement.append(nameDiv, hpDiv);
         this.domElement.click(this.clickHandler);
         return this.domElement;
     }
@@ -56,7 +68,6 @@ var Miles = new Fighter('Miles');
 var Patrick = new Fighter('Patrick');
 var Sean = new Fighter('Sean');
 
-
 var fighterArr = [Cung, Jason, Mike, Miles, Patrick, Sean];
 
 function createFighters() {
@@ -67,8 +78,6 @@ function createFighters() {
     }
 }
 
-
 $(document).ready(function () {
-    console.log('doc is ready');
     createFighters();
 });
