2144d76ee61da2167091f1ef76991cbdc4a91e34
Bill Leonhardt <iamleonhardt@gmail.com>
2016-12-21 18:34:47 -0800

    added students to game

--- script.js
+++ script.js
@@ -1,41 +1,74 @@
 /**
  * Created by bill on 12/9/16.
  */
-$('document').ready(function(){
 
-});
-
+// Constructor for Fighters
 function Fighter(name) {
+    var self = this;
     this.name = name;
     this.div = '';
     this.hitpoints = 100;
-    this.atkDmg = function(maxHit){
+    this.atkDmg = function (maxHit) {
         return Math.floor(Math.random() * maxHit + 1);
     }
-    this.atk = function(opponent){
+    this.atk = function (opponent) {
         var ranDmg = this.atkDmg(10);
         opponent.hitpoints -= ranDmg;
         console.log(this.name + ' hits ' + opponent.name + ' for ' + ranDmg + '.');
         console.log(opponent.name + ' has ' + opponent.hitpoints + ' left.');
-        if(opponent.hitpoints <= 0){
+        if (opponent.hitpoints <= 0) {
             console.log(opponent.name + ' has been knocked out');
             $('#winner').text(this.name + 'wins!');
         }
     }
-    this.specialAtk = function(opponent){
+    this.specialAtk = function (opponent) {
         var ranDmg = this.atkDmg(50);
         opponent.hitpoints -= ranDmg;
         var flavor = [' winds up with all he has and smacks ', ' uses all his rage and smashes ', ' pulls out a secret weapon and hits '];
         var ranFlavor = Math.floor(Math.random() * flavor.length);
         console.log(this.name + flavor[ranFlavor] + opponent.name + ' for ' + ranDmg + '.');
         console.log(opponent.name + ' has ' + opponent.hitpoints + ' left.');
-        if(opponent.hitpoints <= 0){
+        if (opponent.hitpoints <= 0) {
             console.log(opponent.name + ' has been knocked out');
             $('#winner').text(this.name + 'wins!');
         }
     }
+
+    this.clickHandler = function(){
+        self.atk(Sean);
+    }
+
+    this.createDOMElement = function(){
+        this.domElement = $("<div>",{
+            class: 'fighterElement',
+            text: this.name + ' ' + this.hitpoints
+        });
+        this.domElement.click(this.clickHandler);
+        return this.domElement;
+    }
 }
 
-var p1 = new Fighter('BloodSeeker');
-var p2 = new Fighter('Axe');
-var p3 = new Fighter('Tidehunter');
+// List of Fighters
+var Cung = new Fighter('Cung');
+var Jason = new Fighter('Jason');
+var Mike = new Fighter('Mike');
+var Miles = new Fighter('Miles');
+var Patrick = new Fighter('Patrick');
+var Sean = new Fighter('Sean');
+
+
+var fighterArr = [Cung, Jason, Mike, Miles, Patrick, Sean];
+
+function createFighters() {
+    for (var i = 0; i < fighterArr.length; i++) {
+        var thisFighter = fighterArr[i];
+        var thisDOMEle = thisFighter.createDOMElement();
+        $('#gameBoard').append(thisDOMEle);
+    }
+}
+
+
+$(document).ready(function () {
+    console.log('doc is ready');
+    createFighters();
+});
